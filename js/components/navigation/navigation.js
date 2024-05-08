bd05f08fa0b00fbcca5f046edc605a4090c9d1b7
ahmmed-shakil <shakilahmmed252@gmail.com>
2024-05-08 12:32:03 +0600

    Navbar and footer is reusable

--- js/components/navigation/navigation.js
+++ js/components/navigation/navigation.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("DOM content loaded.");
   // Get the menu toggle button and mobile menu by their IDs
-  const menuToggle = document.getElementById("menu-toggle");
-  const mobileMenu = document.getElementById("mobile-menu");
+});
 
-  console.log(menuToggle);
-  // Check if both elements exist before adding an event listener
-  menuToggle.addEventListener("click", function () {
-    console.log("Clicked");
-    mobileMenu.classList.toggle("hidden");
-    console.log("🚀 ~ mobileMenu:", mobileMenu.classList);
-  });
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+console.log(menuToggle);
+// Check if both elements exist before adding an event listener
+menuToggle.addEventListener("click", function () {
+  console.log("Clicked");
+  mobileMenu.classList.toggle("hidden");
+  console.log("🚀 ~ mobileMenu:", mobileMenu.classList);
 });
