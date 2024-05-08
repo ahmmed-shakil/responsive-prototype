2e8c98f91a1aea78165de952a0f5fbf0224c9de0
yeswanth08 <curiousyeswanth003@gmail.com>
2024-05-08 14:08:49 +0530

    header

--- js/components/navigation/navigation.js
+++ js/components/navigation/navigation.js
@@ -3,13 +3,26 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get the menu toggle button and mobile menu by their IDs
 });
 
-const menuToggle = document.getElementById("menu-toggle");
-const mobileMenu = document.getElementById("mobile-menu");
+// const menuToggle = document.getElementById("menu-toggle");
+// const mobileMenu = document.getElementById("mobile-menu");
 
-console.log(menuToggle);
-// Check if both elements exist before adding an event listener
-menuToggle.addEventListener("click", function () {
-  console.log("Clicked");
-  mobileMenu.classList.toggle("hidden");
-  console.log("🚀 ~ mobileMenu:", mobileMenu.classList);
-});
+// console.log(menuToggle);
+// // Check if both elements exist before adding an event listener
+// menuToggle.addEventListener("click", function () {
+//   console.log("Clicked");
+//   mobileMenu.classList.toggle("hidden");
+//   console.log("🚀 ~ mobileMenu:", mobileMenu.classList);
+// });
+
+
+
+// header
+
+function toggleNav() {
+  var navSections = document.getElementById("nav-sections");
+  if (navSections.style.display === "flex") {
+      navSections.style.display = "none";
+  } else {
+      navSections.style.display = "flex";
+  }
+}
