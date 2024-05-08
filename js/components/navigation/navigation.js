0770741018f885a21f8a14d163cdc37b7d75b35a
ahmmed-shakil <shakilahmmed252@gmail.com>
2024-05-08 15:16:39 +0600

    Header updated

--- js/components/navigation/navigation.js
+++ js/components/navigation/navigation.js
@@ -3,26 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get the menu toggle button and mobile menu by their IDs
 });
 
-// const menuToggle = document.getElementById("menu-toggle");
-// const mobileMenu = document.getElementById("mobile-menu");
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
 
-// console.log(menuToggle);
-// // Check if both elements exist before adding an event listener
-// menuToggle.addEventListener("click", function () {
-//   console.log("Clicked");
-//   mobileMenu.classList.toggle("hidden");
-//   console.log("🚀 ~ mobileMenu:", mobileMenu.classList);
-// });
-
-
+console.log(menuToggle);
+// Check if both elements exist before adding an event listener
+menuToggle.addEventListener("click", function () {
+  console.log("Clicked");
+  mobileMenu.classList.toggle("hidden");
+  console.log("🚀 ~ mobileMenu:", mobileMenu.classList);
+});
 
 // header
 
-function toggleNav() {
-  var navSections = document.getElementById("nav-sections");
-  if (navSections.style.display === "flex") {
-      navSections.style.display = "none";
-  } else {
-      navSections.style.display = "flex";
-  }
-}
+// function toggleNav() {
+//   var navSections = document.getElementById("nav-sections");
+//   if (navSections.style.display === "flex") {
+//     navSections.style.visibility = "hidden";
+//   } else {
+//     navSections.style.display = "flex";
+//   }
+// }
