0eef1cf4a5d950a52ce0b5e2c7d4f832ec3b6378
yeswanth08 <curiousyeswanth003@gmail.com>
2024-05-09 12:11:59 +0530

    uploaded about

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -1,23 +1,13 @@
 function loadComponent(componentPath, targetElement) {
-  fetch(componentPath)
-    .then((response) => response.text())
-    .then((content) => {
-      document.querySelector(targetElement).innerHTML = content;
-    })
-    .catch((error) => console.error("Error loading component:", error));
-}
+    fetch(componentPath)
+      .then((response) => response.text())
+      .then((content) => {
+        document.querySelector(targetElement).innerHTML = content;
+      })
+      .catch((error) => console.error("Error loading component:", error));
+  }
+  
 
 
-// Load the navigation JavaScript
-const script = document.createElement("script");
-script.src = "js/components/navigation/navigation.js";
-script.type = "application/javascript";
-document.body.appendChild(script);
-
-// components
-
-loadComponent("components/header.html", "header");
-loadComponent("components/main.html", "main")
-loadComponent("components/footer.html", "footer");
-
-
+  loadComponent("./header.html", "header");
+  loadComponent("./footer.html", "footer");
---
+++ js/loadComponentMain.js
@@ -0,0 +1,23 @@
+function loadComponent(componentPath, targetElement) {
+  fetch(componentPath)
+    .then((response) => response.text())
+    .then((content) => {
+      document.querySelector(targetElement).innerHTML = content;
+    })
+    .catch((error) => console.error("Error loading component:", error));
+}
+
+
+// Load the navigation JavaScript
+const script = document.createElement("script");
+script.src = "js/components/navigation/navigation.js";
+script.type = "application/javascript";
+document.body.appendChild(script);
+
+// components
+
+loadComponent("components/header.html", "header");
+loadComponent("components/main.html", "main")
+loadComponent("components/footer.html", "footer");
+
+
